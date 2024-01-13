dc77027604ea8b48802f30e9800bc41ef8e2f68f
ozdmirr <ozdmirr77@gmail.com>
2024-01-13 22:17:50 +0300

    Redis Pub Sub Examples Added.

---
+++ RedisPubSubExample/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using StackExchange.Redis;
+
+ConnectionMultiplexer connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync("localhost:2000"); //Redis sunucusu için bağlantı cümlesi oluşturuyoruz.
+
+ISubscriber subscriber = connectionMultiplexer.GetSubscriber();
+// Subscriber bağlantısı oluşturduk.
+
+while (true)
+{
+    Console.Write("Message : ");
+    string message = Console.ReadLine();
+
+    await subscriber.PublishAsync("firstChannel", message);
+}
+
+
+
---
+++ RedisSubscriber/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using StackExchange.Redis;
+
+ConnectionMultiplexer connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync("localhost:2000");
+
+ISubscriber subscriber = connectionMultiplexer.GetSubscriber();
+
+await subscriber.SubscribeAsync("firstChannel", (channel, value) =>
+{
+    Console.WriteLine(value);
+});
+
+Console.Read();
